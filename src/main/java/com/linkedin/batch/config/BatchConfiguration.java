905a732f4fd06b3d21c48590bb7b11277e9b9f05
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-08-31 22:38:04 -0300

    Added BatchConfiguration and simple deliverPackageJob and tasklet step

---
+++ src/main/java/com/linkedin/batch/config/BatchConfiguration.java
@@ -0,0 +1,38 @@
+package com.linkedin.batch.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job deliverPackageJob() {
+        return jobBuilderFactory.get("deliverPackageJob")
+                .start(packageItemStep())
+                .build();
+    }
+
+    @Bean
+    public Step packageItemStep() {
+        return stepBuilderFactory.get("packageItemStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("The item has been packaged.");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
