483e652126d7cff2fd19843bcbadef811b1fe0b3
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-09-27 23:35:44 -0300

    Added Order job to read from Database with JDBC paging item reader (multi-thread scenario)

--- src/main/java/com/linkedin/batch/config/JdbcOrderJobConfiguration.java
+++ src/main/java/com/linkedin/batch/config/JdbcOrderJobConfiguration.java
@@ -6,8 +6,12 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +21,12 @@ import javax.sql.DataSource;
 @Configuration
 public class JdbcOrderJobConfiguration {
 
+    private static final String SELECT_CLAUSE = "SELECT order_id, first_name, last_name, email, cost, item_id, item_name, ship_date";
+    private static final String FROM_CLAUSE = "FROM SHIPPED_ORDER";
+
     // It is recommended to have ORDER BY to ensure order of result set, in case we need to restart/resume the job
-    private static final String ORDER_SQL = "SELECT order_id, first_name, last_name, " +
-            "email, cost, item_id, item_name, ship_date " +
-            "FROM SHIPPED_ORDER " +
-            "ORDER BY order_id";
+    private static final String ORDER_SQL = String.join(" ", SELECT_CLAUSE, FROM_CLAUSE, "ORDER BY order_id");
+    private static final int PAGE_CHUNK_SIZE = 10;
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -33,16 +38,24 @@ public class JdbcOrderJobConfiguration {
 
     @Bean
     public Job jdbcOrderProcessingJob() {
-        return jobBuilderFactory.get("jdbcOrderProcessingJob")
-                .start(jdbcOrderProcessingStep())
+        return buildJob("jdbcOrderProcessingJob", jdbcOrderProcessingStep());
+    }
+
+    private Job buildJob(String jobName, Step step) {
+        return jobBuilderFactory.get(jobName)
+                .start(step)
                 .build();
     }
 
     @Bean
     public Step jdbcOrderProcessingStep() {
-        return stepBuilderFactory.get("jdbcOrderProcessingStep")
-                .<Order, Order>chunk(3)
-                .reader(jdbcItemReader())
+        return buildStep("jdbcOrderProcessingStep", jdbcItemReader());
+    }
+
+    private TaskletStep buildStep(String stepName, ItemReader<Order> reader) {
+        return stepBuilderFactory.get(stepName)
+                .<Order, Order>chunk(PAGE_CHUNK_SIZE)
+                .reader(reader)
                 .writer(items -> {
                     System.out.printf("Received a list of  size: %d%n", items.size());
                     items.forEach(System.out::println);
@@ -57,5 +70,39 @@ public class JdbcOrderJobConfiguration {
                 .sql(ORDER_SQL)
                 .rowMapper(new OrderRowMapper())
                 .build();
+    }
+
+    @Bean
+    public Job pagingJdbcOrderProcessingJob() throws Exception {
+        return buildJob("pagingJdbcOrderProcessingJob", pagingJdbcOrderProcessingStep());
+    }
+
+    @Bean
+    public Step pagingJdbcOrderProcessingStep() throws Exception {
+        return buildStep("pagingJdbcOrderProcessingStep", pagingJdbcItemReader());
+    }
+
+    @Bean
+    public ItemReader<Order> pagingJdbcItemReader() throws Exception {
+        return new JdbcPagingItemReaderBuilder<Order>()
+                .dataSource(datasource)
+                .name("jdbcPagingItemReader")
+                .queryProvider(queryProvider())
+                .rowMapper(new OrderRowMapper())
+                .pageSize(PAGE_CHUNK_SIZE) // must be the same as the chunk size
+                .build();
+    }
+
+    @Bean
+    public PagingQueryProvider queryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean factory = new SqlPagingQueryProviderFactoryBean();
+
+        factory.setSelectClause(SELECT_CLAUSE);
+        factory.setFromClause(FROM_CLAUSE);
+        factory.setSortKey("order_id");
+        factory.setDataSource(datasource);
+
+        // Will construct appropriate paging query depending on the underlying database
+        return factory.getObject();
     }
 }
