b24df32d91d6bd307becdd0be44165ddd478704d
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-10-16 23:20:04 -0300

    Added retryJob configuration for later adding retry logic

---
+++ src/main/java/com/linkedin/batch/config/RetryLogicJobConfiguration.java
@@ -0,0 +1,47 @@
+package com.linkedin.batch.config;
+
+import com.linkedin.batch.domain.Order;
+import com.linkedin.batch.domain.TrackedOrder;
+import com.linkedin.batch.exception.OrderProcessingException;
+import com.linkedin.batch.listener.CustomSkipListener;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RetryLogicJobConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job retryJob(Step retryStep) {
+        return jobBuilderFactory.get("retryJob")
+                .start(retryStep)
+                .build();
+    }
+
+    @Bean
+    public Step retryStep(
+            ItemReader<Order> pagingJdbcItemReader,
+            ItemProcessor<Order, TrackedOrder> freeShippingProcessor,
+            ItemWriter<TrackedOrder> jsonWriter) {
+
+        return stepBuilderFactory.get("retryStep")
+                .<Order, TrackedOrder>chunk(10)
+                .reader(pagingJdbcItemReader)
+                .processor(freeShippingProcessor)
+                .writer(jsonWriter)
+                .build();
+    }
+}
