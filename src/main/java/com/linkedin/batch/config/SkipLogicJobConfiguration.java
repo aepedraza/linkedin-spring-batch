882c84b87e62b2a86cf5bfaab11f2f899f744360
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-10-16 23:01:10 -0300

    Added skipJob configuration for later adding skip logic

---
+++ src/main/java/com/linkedin/batch/config/SkipLogicJobConfiguration.java
@@ -0,0 +1,45 @@
+package com.linkedin.batch.config;
+
+import com.linkedin.batch.domain.Order;
+import com.linkedin.batch.domain.TrackedOrder;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SkipLogicJobConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job skipJob(Step skipStep) {
+        return jobBuilderFactory.get("skipJob")
+                .start(skipStep)
+                .build();
+    }
+
+    @Bean
+    public Step skipStep(
+            ItemReader<Order> pagingJdbcItemReader,
+            ItemProcessor<Order, TrackedOrder> freeShippingProcessor,
+            ItemWriter<TrackedOrder> jsonWriter) {
+
+        return stepBuilderFactory.get("skipStep")
+                .<Order, TrackedOrder>chunk(10)
+                .reader(pagingJdbcItemReader)
+                .processor(freeShippingProcessor)
+                .writer(jsonWriter)
+                .build();
+    }
+}
